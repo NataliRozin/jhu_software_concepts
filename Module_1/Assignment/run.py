844cce12424f1309d2d422678b2508fa426cd9d5
NataliRozin <nrozin1@jh.edu>
2025-05-24 18:34:22 +0200

    reorganization

--- Module_1/Assignment/run.py
+++ Module_1/Assignment/run.py
@@ -1,7 +1,9 @@
-from board import create_app
+# an object of WSGI application
+from website import create_app
 
-# Build the app
+# building the app
 app = create_app()
 
 if __name__ == "__main__":
+    # run the application
     app.run(host="0.0.0.0", port=8000, debug=True)
