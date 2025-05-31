23a88f6a8f29bf207bcdbfd4af6cf8bb5de8bcc9
NataliRozin <nrozin1@jh.edu>
2025-05-31 16:38:05 +0200

    added scores

--- module_2/clean.py
+++ module_2/clean.py
@@ -17,18 +17,33 @@ def clean_data(rows, base_url):
         # Store columns data
         cols = row.find_all("td")
 
-        entry = re.findall(r'/result/\d+', str(row))
+        match = re.findall(r'/result/\d+', str(row))
+        if match:
+            entry = match[0]
 
         if len(cols) >= 4:
             full_program = cols[1].text.strip().split('\n\n\n\n')
 
-            info[entry[0].replace("/result/", "")] = {"program": f"{cols[0].text.strip()}, {full_program[0]}",
+            info[entry.replace("/result/", "")] = {"program": f"{cols[0].text.strip()}, {full_program[0]}",
                                                       "publish date": cols[2].text.strip(),
-                                                      "url": base_url + entry[0],
+                                                      "url": base_url + entry,
                                                       "status": cols[3].text.strip(),
                                                       "term": "",
                                                       "degree": full_program[1]
                                                       }
+        elif len(cols) == 1:
+            GPA = re.findall(r'\bGPA\s+\d\.\d{1,2}', str(row), re.IGNORECASE)
+            if GPA:
+                info[entry.replace("/result/", "")].update({"GPA": GPA[0].upper().replace("GPA", "").strip()})
+            
+            GRE = re.findall(r'\bGRE\s+\d+', str(row), re.IGNORECASE)
+            if GRE:
+                info[entry.replace("/result/", "")].update({"GRE": GRE[0].upper().replace("GRE", "").strip()})
+
+            GRE_V = re.findall(r'\bGRE\s+V\s+\d+', str(row), re.IGNORECASE)
+            if GRE_V:
+                info[entry.replace("/result/", "")].update({"GRE V": re.sub(r'[^\d]', '', GRE_V[0])})
+
     return info
 
 def save_data(data, filename='gradcafe_data.json'):
