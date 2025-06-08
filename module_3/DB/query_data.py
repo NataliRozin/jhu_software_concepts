8ab8c800ae0f3729d6764ac1f0f836e18e98f96b
NataliRozin <nrozin1@jh.edu>
2025-06-08 21:47:11 +0200

    updated design and query_data.py

--- module_3/DB/query_data.py
+++ module_3/DB/query_data.py
@@ -9,9 +9,11 @@ class Query:
     def count_fall25_entries(self):
         # How many entries do you have in your database who have applied for Fall 2025?
         query = """
-                SELECT COUNT(*) FROM applicants
+                SELECT COUNT(term) FROM applicants
                 WHERE term = 'Fall 2025';
                 """
+        query = """
+                SELECT COUNT(*) FROM applicants"""
         try:
             # Open cursor to perform database operations
             with self.connection.cursor() as cur:
@@ -27,7 +29,7 @@ class Query:
         query = """
                 SELECT ROUND(
                  100.0 * COUNT(us_or_international)
-                 FILTER (WHERE us_or_international = 'International') / COUNT(*), 2) AS international_percentage
+                 FILTER (WHERE us_or_international = 'International') / COUNT(*), 2)
                 FROM applicants;
                 """
         try:
@@ -50,20 +52,25 @@ class Query:
                 AND GPA BETWEEN 0 AND 4.0
                 AND GRE BETWEEN 260 AND 340
                 AND GRE_V BETWEEN 130 AND 170
-                AND GRE_AW BETWEEN 0 AND 6;
+                AND GRE_AW BETWEEN 0 AND 6
+                AND MOD((GRE_AW * 10)::INT, 5) = 0;
                 """
         try:
             # Open cursor to perform database operations
             with self.connection.cursor() as cur:
                 cur.execute(query)
                 result = cur.fetchall()
-                return list(round(result[0], 2))
+
+                # Extract the tuple, round each element to 2 decimals, and return as list
+                rounded_list = [round(x, 2) for x in result[0]]
+
+                return rounded_list
             
         except Exception as e:
             print(f"Error executing query: {e}")
 
     def average_gpa_american_fall25(self):
-        # What is their average GPA of American students in Fall 2025?
+        # What is the average GPA of American students in Fall 2025?
         query = """
                 SELECT AVG(GPA) FROM applicants
                 WHERE term = 'Fall 2025'
@@ -83,9 +90,11 @@ class Query:
     def count_accepted_fall25(self):
         # What percent of entries for Fall 2025 are Acceptances (to two decimal places)?
         query = """
-                SELECT COUNT(status) FROM applicants
-                WHERE term = 'Fall 2025'
-                AND status LIKE '%Accepted%';
+                SELECT ROUND(
+                 100.0 * COUNT(status)
+                 FILTER (WHERE status LIKE '%Accepted%' AND term = 'Fall 2025') /
+                 COUNT(*), 2)
+                FROM applicants;
                 """
         try:
             # Open cursor to perform database operations
