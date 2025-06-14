c49a5753c95f8d2e9eae1fcae629c65f5915d2d2
NataliRozin <nrozin1@jh.edu>
2025-06-14 14:04:38 +0200

    updated pytest.ini - now includes Pizza mark

--- module_4/Tests/test_pizza.py
+++ module_4/Tests/test_pizza.py
@@ -2,22 +2,6 @@ import pytest # type: ignore
 from src.order import Order # type: ignore
 from src.pizza import Pizza # type: ignore
 
-# Fixture to create an order with two pizzas
-@pytest.fixture
-def sample_order():
-    """
-    Fixture to create an Order instance with two pizzas added.
-
-    Returns:
-        Order: An order containing two pizzas:
-            - Pizza 1: thin crust, pesto sauce, mozzarella, mushrooms
-            - Pizza 2: thick crust, marinara sauce, mozzarella, mushrooms
-    """
-    order_1 = {'crust': 'thin', 'sauce': ['pesto'], 'cheese': 'mozzarella', 'toppings': ['mushrooms']}
-    order_2 = {'crust': 'thick', 'sauce': ['marinara'], 'cheese': 'mozzarella', 'toppings': ['mushrooms']}
-    
-    return order_1, order_2
-
 @pytest.mark.pizza_mark
 def test_pizza_initialization(_, mock_pizza):
     # Create a new Pizza instance
