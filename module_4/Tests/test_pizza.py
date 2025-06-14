a5243efafb71002eb494cb89d8a0f8b553078eef
NataliRozin <nrozin1@jh.edu>
2025-06-14 14:17:37 +0200

    updated Pizza.__init__() test

--- module_4/Tests/test_pizza.py
+++ module_4/Tests/test_pizza.py
@@ -12,16 +12,43 @@ def test_pizza_initialization():
         toppings=['mushrooms']
     )
 
-    # Assert that crust is 'thin'
+    #--- CRUST ---#
+    # Assert that crust is a string
+    assert isinstance(pizza.crust, str)
+
+    # Assert crust is 'thin'
     assert pizza.crust == 'thin'
 
+    #--- SUACE ---#
     # Assert that sauce is a list containing one variable - 'pesto'
     assert isinstance(pizza.sauce, list)
+    
+    # Assert all elements are strings
+    assert all(isinstance(s, str) for s in pizza.sauce)
+
+    # Assert sauce includes 'pesto'
     assert pizza.sauce == ['pesto']
+
+    #--- CHEESE ---#
+    # Assert that cheese is a string
+    assert isinstance(pizza.crust, str)
 
     # Assert that cheese is 'mozzarella'
     assert pizza.cheese == 'mozzarella'
 
-    # Assert that toppings is a list containing one variable - 'mushrooms'
+    #--- TOPPINGS ---#
+    # Assert that toppings is a list
     assert isinstance(pizza.toppings, list)
+
+    # Assert all elements are strings
+    assert all(isinstance(t, str) for t in pizza.toppings)
+
+    # Assert toppings include 'mushrooms'
     assert pizza.toppings == ['mushrooms']
+
+    #--- COST ---#
+    # Assert cost is an integer
+    assert isinstance(pizza._cost, int)
+    
+    # Assert cost is higher than 0
+    assert pizza._cost > 0
