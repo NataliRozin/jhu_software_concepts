84d8cb9112432112bbdccb3c2f5529aa41e1bfec
NataliRozin <nrozin1@jh.edu>
2025-06-15 19:54:18 +0200

    updated order.py documentation

--- module_4/src/order.py
+++ module_4/src/order.py
@@ -6,6 +6,7 @@ This module contains the ``Order`` class, which represents a customer order in a
 pizza ordering system.
 
 The ``Order`` class allows:
+
 * adding pizzas to the order
 * calculating the total cost, 
 * marking the order as paid.
