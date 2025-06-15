2d22cfbdb1c96d6a9dae1aa9705187a7d1fdc4f9
NataliRozin <nrozin1@jh.edu>
2025-06-16 00:07:51 +0200

    updated sphinx files

--- module_4/docs/conf.py
+++ module_4/docs/conf.py
@@ -9,8 +9,8 @@
 import sys
 from pathlib import Path
 
-src_path = Path(__file__).resolve().parents[1] / "src"
-sys.path.insert(0, str(src_path))
+project_root = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(project_root / "src"))
 
 project = 'Module_4_PizzaOrder'
 copyright = '2025, Natali R.'
--- module_4/docs/order.rst
+++ module_4/docs/order.rst
@@ -1,7 +1,7 @@
 Order Module
 ************
 
-.. automodule:: order
+.. automodule:: module_4.order
    :members:
    :special-members: __init__, __str__
    :show-inheritance:
